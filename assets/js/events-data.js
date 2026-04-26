cf6dff40f1ce16989018e7a1c7b93261382de278
Rogerio O. Ferraz <rogerio.o.ferraz@gmail.com>
2026-04-26 16:57:27 -0300

    Updated events session
    Archive the past Piquenique event
    
    Signed-off-by: Rogerio O. Ferraz <rogerio.o.ferraz@gmail.com>

--- assets/js/events-data.js
+++ assets/js/events-data.js
@@ -1,11 +1,1 @@
-window.EVENTS_DATA = Object.freeze([
-  {
-    file: "3-piquenique-cientec-2026.jpeg",
-    title: "Piquenique no Parque CienTec",
-    description:
-      "Aproveitem para convidar e levar as crianças (vizinhos, sobrinhos, netos) para brincar e compartilhar tempo juntos.<br>" +
-      "<strong>Local de encontro:</strong> Entrada do Parque às 9:00<br>" +
-      "Av. Miguel Estefno, 4200 - São Paulo-SP<br>" +
-      '<a class="btn btn-primary" href="https://maps.app.goo.gl/bCqDB2y4poRDVUit5" target="_blank" rel="noopener noreferrer">Abrir Google Maps</a>',
-  },
-]);
+window.EVENTS_DATA = Object.freeze([]);
